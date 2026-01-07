b51aa53d92ef4c6b544e3adcdc6929db0b1a4e47
Itachi917 <87574052+Itachi917@users.noreply.github.com>
2026-01-08 01:44:50 +0400

    Refactor Prisma client initialization
    Refactor Prisma client initialization to use a singleton pattern and improve logging.

--- student-platform/lib/db.ts
+++ student-platform/lib/db.ts
@@ -1,18 +1,17 @@
-
-import { PrismaClient } from "@prisma/client"
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
-
-export default prisma
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+export default prisma;
